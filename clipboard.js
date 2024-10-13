38b6dbad3c9a2274197a8ab758e3fabf7138977f
Marcin Er <mar.rudzki@gmail.com>
2024-10-13 12:24:29 +0200

    add HTML preview

--- clipboard.js
+++ clipboard.js
@@ -1,6 +1,16 @@
 const pasteTextButton = document.getElementById('paste-text-button');
-const resetTextButton = document.getElementById('reset-text-button');
 const textField = document.getElementById('text');
+const resetTextButton = document.getElementById('reset-text-button');
+
+const pasteHTMLButton = document.getElementById('paste-html-button');
+const htmlField = document.getElementById('html');
+const resetHTMLButton = document.getElementById('reset-html-button');
+const htmlOutput = document.getElementById('html-output');
+
+resetHTMLButton.addEventListener('click', () => {
+  htmlField.value = '';
+  htmlOutput.innerHTML = '';
+});
 
 resetTextButton.addEventListener('click', () => {
   textField.value = '';
@@ -12,5 +22,18 @@ pasteTextButton.addEventListener('click', async () => {
     textField.value += text;
   } catch (error) {
     console.error(error);
+  }
+});
+
+pasteHTMLButton.addEventListener('click', async () => {
+  const items = await navigator.clipboard.read();
+
+  for (const item of items) {
+    if (item.types.includes('text/html')) {
+      const blob = await item.getType('text/html');
+      const html = await blob.text();
+      htmlField.value = html;
+      htmlOutput.innerHTML = html;
+    }
   }
 });
